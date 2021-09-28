b1071a83e8d5c347f0974feafa5ffcac702a98e2
Ryan <kotard8899@gmail.com>
2021-09-28 17:31:14 +0800

    add query

--- pages/index.js
+++ pages/index.js
@@ -3,21 +3,22 @@ import Header from '@components/Header'
 import Footer from '@components/Footer'
 import { useEffect } from 'react'
 
-function Home({ data }) {
+function Home({ data, query }) {
   return (
     <div className="container">
       {data.data[0].address}
+      {query && Object.keys(query).map((k, i) => <div key={i}>{query[k]}</div>)}
     </div>
   )
 }
 
-export async function getServerSideProps() {
+export async function getServerSideProps({ query }) {
   // Fetch data from external API
   const res = await fetch(`https://api.artion.io/info/getcollections`)
   const data = await res.json()
-
+  // console.log(query)
   // Pass data to the page via props
-  return { props: { data } }
+  return { props: { data, query } }
 }
 
 export default Home
