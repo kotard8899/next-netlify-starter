9dc55b53b57cd78d13efdc5922f6af8ad812619b
Ryan <kotard8899@gmail.com>
2021-09-28 17:08:04 +0800

    init

--- pages/index.js
+++ pages/index.js
@@ -1,23 +1,23 @@
 import Head from 'next/head'
 import Header from '@components/Header'
 import Footer from '@components/Footer'
+import { useEffect } from 'react'
 
-export default function Home() {
+function Home({ data }) {
   return (
     <div className="container">
-      <Head>
-        <title>Next.js Starter!</title>
-        <link rel="icon" href="/favicon.ico" />
-      </Head>
-
-      <main>
-        <Header title="Welcome to my app!" />
-        <p className="description">
-          Get started by editing <code>pages/index.js</code>
-        </p>
-      </main>
-
-      <Footer />
+      {data.data[0].address}
     </div>
   )
 }
+
+export async function getServerSideProps() {
+  // Fetch data from external API
+  const res = await fetch(`https://api.artion.io/info/getcollections`)
+  const data = await res.json()
+
+  // Pass data to the page via props
+  return { props: { data } }
+}
+
+export default Home
